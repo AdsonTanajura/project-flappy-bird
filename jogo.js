6e7d54fe8da7f12996106c649b92f08cd22b64fa
Adson <adson.tanajura@gmail.com>
2023-11-17 19:03:05 -0300

    base

---
+++ jogo.js
@@ -0,0 +1,8 @@
+console.log("Flappy Bird");
+
+const sprites = new Image();
+sprites.src = "./assets/sprites.png";
+
+const canvas = document.querySelector("canvas");
+
+const contexto = canvas.getContext("2d");
