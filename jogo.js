3e33e2bf82d2827daeb37689fbb8e582c82440bb
Adson <adson.tanajura@gmail.com>
2023-11-21 19:59:33 -0300

    add backgourd and flor

--- jogo.js
+++ jogo.js
@@ -24,10 +24,85 @@ const flappybird = {
     }
 };
 
+const background = {
+    spriteX: 390,
+    spriteY: 0,
+    largura: 665,
+    altura: 203,
+    localX: 0,
+    localY: canvas.height - 203,
+    
+    desenha() {
+        contexto.fillStyle = '#70c5ce'
+        contexto.fillRect(0,0, canvas.width, canvas.height)
+
+        contexto.drawImage(
+            sprites,
+            background.spriteX,
+            background.spriteY,
+            background.largura,
+            background.altura,
+            background.localX,
+            background.localY,
+            background.largura,
+            background.altura,
+        );
+        
+        contexto.drawImage(
+            sprites,
+            background.spriteX,
+            background.spriteY,
+            background.largura,
+            background.altura,
+            (background.localX + background.altura),
+            background.localY,
+            background.largura,
+            background.altura,
+        );
+    }
+};
+
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 223,
+    altura: 721,
+    localX: 0,
+    localY: canvas.height - 112,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            chao.spriteX,
+            chao.spriteY,
+            chao.largura,
+            chao.altura,
+            chao.localX,
+            chao.localY,
+            chao.largura,
+            chao.altura,
+        );
+
+        contexto.drawImage(
+            sprites,
+            chao.spriteX,
+            chao.spriteY,
+            chao.largura,
+            chao.altura,
+            (chao.localX + chao.largura),
+            chao.localY,
+            chao.largura,
+            chao.altura,
+        );
+        
+    }
+};
+
 function loop() {
 
+    background.desenha();
+    chao.desenha();
     flappybird.desenha();
-    
+
      requestAnimationFrame(loop);
 
  }
