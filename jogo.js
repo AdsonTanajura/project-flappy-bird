f46eb65aa7c7e9f03b71d5e2674c2ea053cf7f66
Adson <adson.tanajura@gmail.com>
2023-11-21 18:28:28 -0300

    add flappy bird

--- jogo.js
+++ jogo.js
@@ -1,8 +1,35 @@
-console.log("Flappy Bird");
+console.log('Flappy Bird');
 
 const sprites = new Image();
-sprites.src = "./assets/sprites.png";
+sprites.src = './assets/sprites.png';
 
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
 
-const contexto = canvas.getContext("2d");
+const flappybird = {
+    spriteX: 0,
+    spriteY: 0,
+    largura: 34,
+    altura: 24,
+    localX: 10,
+    localY: 50,
+    desenha() {
+        contexto.drawImage(
+            sprites,
+            flappybird.spriteX, flappybird.spriteY,
+            flappybird.largura, flappybird.altura,
+            flappybird.localX, flappybird.localY,
+            flappybird.largura, flappybird.altura,
+         );
+    }
+};
+
+function loop() {
+
+    flappybird.desenha();
+    
+     requestAnimationFrame(loop);
+
+ }
+
+ loop();
